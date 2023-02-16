0748f86c28bd20fde4e8df8b3439148e7553b2db
trankhaidz <2121050611@student.humg.edu.vn>
2023-02-16 10:10:35 +0700

    Update TranQuangKhai_App.java

--- TranQuangKhai_App.java
+++ TranQuangKhai_App.java
@@ -2,6 +2,10 @@
 public class TranQuangKhai_App {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("Tran Quang Khai Hoc Java");
+=======
+		System.out.println("Trần Quang Khải Học Java");
+>>>>>>> Stashed changes
 	}
 }
